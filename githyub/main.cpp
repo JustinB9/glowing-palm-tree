5a7ccf32283b8e0c67e40fb307caf7507ecd95d9
JustinB9 <justinbryan9@gmail.com>
2017-01-15 21:45:15 -0700

    Initial Commit

---
+++ githyub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  githyub
+//
+//  Created by Justin Bryan on 1/15/17.
+//  Copyright © 2017 Justin Bryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
